7d7bbb8010cd61c7df5b82a242ee2339d0e26b35
nontasan <nontasan.ni@princhealth.com>
2024-01-09 16:48:22 +0700

    add URL

--- src/hello.js
+++ src/hello.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', function (req, res) {
-	res.send('<center><h1>Hello World! from  ' + String(os.hostname()) + '</h1></center>')
+	res.send('<center><h1>Hello World! MY NAME IS NONZA >>>> from  ' + String(os.hostname()) + '</h1></center>')
 })
 
 app.listen(port, () => console.log(`Hello World! listening on port ${port}!`))
